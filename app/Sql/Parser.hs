8d676e035b8966814372689d15fa17932ce1ebde
ludihan <65617704+ludihan@users.noreply.github.com>
2025-05-05 00:19:01 -0300

    getting crazy

--- app/Sql/Parser.hs
+++ app/Sql/Parser.hs
@@ -1,40 +1,71 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Sql.Parser where
 
+parse :: Parser a -> String -> Either String (a, ParserState)
+parse parser cs = runParser parser $ ParserState cs 1 1
+
 newtype Parser a = Parser
-    { runParser :: String -> Either String (a, String)
+    { runParser :: ParserState -> Either String (a, ParserState)
     }
+
+data ParserState = ParserState
+    { input :: String
+    , line :: Int
+    , column :: Int
+    }
+    deriving (Show)
 
 instance Functor Parser where
     -- fmap :: (a -> b) -> Parser a -> Parser b
     fmap f (Parser p) = Parser $
-        \input -> do
-            (x, rest) <- p input
+        \s -> do
+            (x, rest) <- p s
             Right (f x, rest)
 
 instance Applicative Parser where
     -- pure :: a -> Parser a
     pure x = Parser $
-        \input ->
-            Right (x, input)
+        \s ->
+            Right (x, s)
 
     -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
     (Parser p1) <*> (Parser p2) = Parser $
-        \input -> do
-            (f, rest) <- p1 input
+        \s -> do
+            (f, rest) <- p1 s
             (x, rest') <- p2 rest
             pure (f x, rest')
 
 instance Monad Parser where
     -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
     Parser p >>= f = Parser $
-        \input -> do
-            (a, rest) <- p input
+        \s -> do
+            (a, rest) <- p s
             runParser (f a) rest
 
 item :: Parser Char
 item = Parser $
-    \case
-        "" -> Left "empty"
-        (c : cs') -> Right (c, cs')
+    \s -> case input s of
+        "" ->
+            Left $
+                "didnt find any characters in line "
+                    ++ show (line s)
+                    ++ " at column "
+                    ++ show (column s)
+        (c : rest) ->
+            let
+                line' =
+                    if c == '\n' then line s + 1 else line s + 0
+                column' =
+                    (column s + 1)
+             in
+                Right
+                    ( c
+                    , s
+                        { input = rest
+                        , line = line'
+                        , column = column'
+                        }
+                    )
+
+parseSelect = do
+    _ <- item
+    item
